6313c721ba37f17432c6a3b4873d7dbd8cc51cee
Daisuke Bekki <bekki@is.ocha.ac.jp>
2021-02-11 04:21:33 +0900

    added zeros'

--- src/Torch/Tensor/TensorFactories.hs
+++ src/Torch/Tensor/TensorFactories.hs
@@ -3,11 +3,12 @@
 module Torch.Tensor.TensorFactories (
   asTensor'',
   randIO',
-  randnIO'
+  randnIO',
+  zeros'
 ) where
 
 import Torch.Tensor (Tensor(..),TensorLike(..))
-import Torch.TensorFactories (randIO,randnIO)
+import Torch.TensorFactories (randIO,randnIO,zeros)
 import Torch.TensorOptions (defaultOpts,withDevice)
 import Torch.Device (Device(..))
 
@@ -19,3 +20,6 @@ randIO' dev shape = randIO shape $ withDevice dev defaultOpts
 
 randnIO' :: Device -> [Int] -> IO Tensor
 randnIO' dev shape = randnIO shape $ withDevice dev defaultOpts
+
+zeros' :: Device -> [Int] -> IO Tensor
+zeros' dev shape = zeros shape $ withDevice dev defaultOpts
