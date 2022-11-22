be99de8bb38bb400f72747bec75b136986e68723
Sora Tagami <loslonbervene.1923@icloud.com>
2022-11-22 15:43:00 +0900

    add RNN

---
+++ src/Torch/Layer/RNN.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Torch.Layer.RNN (
+  RNNHypParams(..),
+  RNNParams(..),
+  rnnLayer,
+) where
+
+import Torch
+import Torch.Layer.Linear (LinearHypParams(..),LinearParams(..),linearLayer)
+import GHC.Generics
+
+data RNNHypParams = RNNHypParams
+  { 
+    dev :: Device,
+    inputDim :: Int,
+    hiddenDim :: Int
+  }
+
+data RNNParams = RNNParams
+  { inh :: LinearParams,
+    hh :: LinearParams
+  } deriving (Show, Generic, Parameterized)
+
+instance Randomizable RNNHypParams RNNParams where
+  sample RNNHypParams {..} =
+    RNNParams
+      <$> sample (LinearHypParams dev inputDim hiddenDim)
+      <*> sample (LinearHypParams dev hiddenDim hiddenDim)
+
+rnnCell ::
+  RNNParams ->
+  -- h_t-1
+  Tensor  ->
+  -- x_t
+  Tensor ->
+  -- h_t
+  Tensor
+rnnCell RNNParams {..} ht xt =
+  Torch.tanh ( linearLayer inh xt + linearLayer hh ht)
+
+rnnLayer ::
+  -- | model
+  RNNParams ->
+  -- | h_0
+  Tensor ->
+  -- | input
+  [Tensor] -> 
+  -- | (output, h_last)
+  ([Tensor], Tensor)
+rnnLayer model h0 input =
+  let hidden = scanl (rnnCell model) h0 input
+  in (tail hidden, last hidden)
