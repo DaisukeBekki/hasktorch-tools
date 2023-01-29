ad5bd1ac98dc56b212c3543b301ff8d61cff5534
Daisuke Bekki <bekki@is.ocha.ac.jp>
2023-01-29 23:32:24 +0900

    added pytorch-style LSTM layer

--- src/Torch/Layer/LSTM.hs
+++ src/Torch/Layer/LSTM.hs
@@ -1,20 +1,19 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Torch.Layer.LSTM (
-  LstmHypParams(..),
-  LstmParams(..),
-  lstmCell,
-  lstmLayers
-  --lstmLayers'
+  LstmHypParams(..)
+  , LstmParams(..)
+  , lstmLayers
   ) where 
 
 import Prelude hiding (tanh) 
 import GHC.Generics              --base
-import Data.List (scanl',foldl') --base
-import Control.Monad (forM)      --base
+import Data.Maybe (isJust)    --base
+import Data.List (scanl',foldl',scanr) --base
+import Control.Monad (forM,unless)      --base
 --hasktorch
-import Torch.Tensor       (Tensor(..))
-import Torch.Functional   (Dim(..),sigmoid,cat,stack)
+import Torch.Tensor       (Tensor(..),shape,select,sliceDim)
+import Torch.Functional   (Dim(..),sigmoid,cat,stack,dropout)
 import Torch.Device       (Device(..))
 import Torch.NN           (Parameterized(..),Randomizable(..),Parameter,sample)
 import Torch.Autograd (IndependentTensor(..),makeIndependent)
@@ -23,9 +22,15 @@ import Torch.Tensor.TensorFactories (randnIO')
 import Torch.Layer.Linear (LinearHypParams(..),LinearParams(..),linearLayer)
 
 data LstmHypParams = LstmHypParams {
-  dev :: Device,
-  stateDim :: Int,
-  numOfLayers :: Int
+  dev :: Device
+  , bidirectional :: Bool 
+  , input_size :: Int  -- ^ The number of expected features in the input x
+  , hidden_size :: Int -- ^ The number of features in the hidden state h
+  , num_layers :: Int     -- ^ Number of recurrent layers
+  , dropoutProb :: Maybe Double  -- ^ If non-zero, introduces a Dropout layer on the outputs of each LSTM layer except the last layer, with dropout probability equal to dropout.
+  -- , bias :: Bool  -- ^ If False, then the layer does not use bias weights b_ih and b_hh.
+  -- , batch_first :: Bool -- ^ If True, then the input and output tensors are provided as (batch, seq, feature) instead of (seq, batch, feature).
+  -- , proj_size :: Float -- ^ If > 0, will use LSTM with projections of corresponding size.
   } deriving (Eq, Show)
 
 data SingleLstmParams = SingleLstmParams {
@@ -36,36 +41,10 @@ data SingleLstmParams = SingleLstmParams {
     } deriving (Show, Generic)
 instance Parameterized SingleLstmParams
 
-data LstmParams = LstmParams {
-  lstmParams :: [SingleLstmParams],
-  initialParams :: [(Parameter,Parameter)]
-  } deriving (Show, Generic)
-
-instance Parameterized LstmParams
-instance Randomizable LstmHypParams LstmParams where
-  sample LstmHypParams{..} = do
-    let x_Dim = stateDim
-        h_Dim = stateDim
-        c_Dim = stateDim
-        xh_Dim = x_Dim + h_Dim
-    LstmParams
-      <$> forM [1..numOfLayers] (\_ ->
-        SingleLstmParams 
-          <$> sample (LinearHypParams dev xh_Dim c_Dim)
-          <*> sample (LinearHypParams dev xh_Dim c_Dim)
-          <*> sample (LinearHypParams dev xh_Dim c_Dim)
-          <*> sample (LinearHypParams dev xh_Dim h_Dim)
-          )
-      <*> forM [1..(numOfLayers-1)] (\_ ->
-        (\x y -> (x,y)) -- IO(a)->IO(b->(a,b))
-          <$> (makeIndependent =<< randnIO' dev [c_Dim]) 
-          <*> (makeIndependent =<< randnIO' dev [x_Dim])
-          )
-
 lstmCell :: SingleLstmParams 
-  -> (Tensor,Tensor) -- ^ (ct,ht) 
+  -> (Tensor,Tensor) -- ^ (ht,ct) 
   -> Tensor          -- ^ xt
-  -> (Tensor,Tensor) -- ^ (ct',ht')
+  -> (Tensor,Tensor) -- ^ (ht',ct')
 lstmCell SingleLstmParams{..} (ht,ct) xt =
   let xt_ht = cat (Dim 0) [xt,ht]
       ft = sigmoid $ linearLayer forgetGate $ xt_ht
@@ -74,41 +53,88 @@ lstmCell SingleLstmParams{..} (ht,ct) xt =
       ct' = (ft * ct) + (it * cant)
       ot = sigmoid $ linearLayer outputGate $ xt_ht
       ht' = ot * (tanh' ct')
-  in (ht', ct')
+  in (ht',ct')
   where -- | HACK : Torch.Functional.tanhとexpが `Segmentation fault`になるため
     tanh' x = 2 * (sigmoid $ 2 * x) - 1
 
+data LstmParams = LstmParams {
+  firstLstmParams :: SingleLstmParams -- ^ a model for the first LSTM layer
+  , restLstmParams :: [SingleLstmParams] -- ^ models for the rest of LSTM layers
+  } deriving (Show, Generic)
+instance Parameterized LstmParams
+
+-- (makeIndependent =<< randnIO' dev [c_Dim]) 
+instance Randomizable LstmHypParams LstmParams where
+  sample LstmHypParams{..} = do
+    let x_Dim = input_size
+        h_Dim = hidden_size
+        c_Dim = hidden_size
+        xh1_Dim = x_Dim + h_Dim
+        xh2_Dim = if bidirectional
+                    then (2 * x_Dim) + h_Dim
+                    else xh1_Dim
+    LstmParams
+      <$> (SingleLstmParams
+            <$> sample (LinearHypParams dev xh1_Dim c_Dim)
+            <*> sample (LinearHypParams dev xh1_Dim c_Dim)
+            <*> sample (LinearHypParams dev xh1_Dim c_Dim)
+            <*> sample (LinearHypParams dev xh1_Dim h_Dim)
+            )
+      <*> forM [2..num_layers] (\_ ->
+        SingleLstmParams 
+          <$> sample (LinearHypParams dev xh2_Dim c_Dim)
+          <*> sample (LinearHypParams dev xh2_Dim c_Dim)
+          <*> sample (LinearHypParams dev xh2_Dim c_Dim)
+          <*> sample (LinearHypParams dev xh2_Dim h_Dim)
+          )
+
 -- | inputのlistから、(cellState,hiddenState=output)のリストを返す
 -- | scanl' :: ((h,c) -> input -> (h',c')) -> (h0,c0) -> [input] -> [(hi,ci)]
-lstmLayer :: Bool       -- ^ if BiLSTM then True else False
-  -> SingleLstmParams   -- ^ hyper params
-  -> (Tensor,Tensor)    -- ^ A pair of vectors (h0,c0)
+singleLstmLayer :: Bool -- ^ True if BiLSTM, False otherwise
+  -> Int                -- ^ hidden_size 
+  -> SingleLstmParams   -- ^ params
+  -> (Tensor,Tensor)    -- ^ A pair (h0,c0) of shape (hidden_size) or (2,hidden_size)
   -> [Tensor]           -- ^ an input layer
-  -> [(Tensor,Tensor)]  -- ^ the list of (hi,ci) pairs
-lstmLayer ifBiLstm params (h0,c0) inputs = 
-  let firstLayer = tail $ scanl' (lstmCell params) (h0,c0) inputs in
-    if ifBiLstm -- | (c0,h0)は除くためtailを取る
-      then reverse $ tail $ scanl' (lstmCell params) (last firstLayer) $ reverse $ snd $ unzip firstLayer
-      else firstLayer
+  -> IO [Tensor]        -- ^ [forward_hi] or [forward_hi+backward_hi]
+singleLstmLayer isBiLSTM stateDim params (h0,c0) inputs = do
+  let h0shape = shape h0
+      c0shape = shape c0
+  if isBiLSTM -- check the well-formedness of the shapes of h0 and c0
+    then do -- BiLSTM
+      unless ((h0shape == [2,stateDim]) && (c0shape == [2,stateDim])) $
+        ioError $ userError $ "illegal h0 or c0 shape: " ++ (show h0shape) ++ " or " ++ (show c0shape)
+      let h0f = select 0 0 h0
+          h0b = select 0 1 h0
+          c0f = select 0 0 c0
+          c0b = select 0 1 c0
+          -- | 以下、(c0,h0)は除くためtailを取る
+          forwardLayer = fst $ unzip $ tail $ scanl' (lstmCell params) (h0f,c0f) inputs 
+          backwardLayer = fst $ unzip $ init $ scanr (flip $ lstmCell params) (h0b,c0b) inputs 
+      return $ map (\(f,b)-> cat (Dim 0) [f,b]) $ zip forwardLayer backwardLayer
+    else do -- LSTM
+      unless ((h0shape == [stateDim]) && (c0shape == [stateDim])) $
+        ioError $ userError $ "illegal h0 or c0 shape: " ++ (show h0shape) ++ " or " ++ (show c0shape)
+      return $ fst $ unzip $ tail $ scanl' (lstmCell params) (h0,c0) inputs -- | (c0,h0)は除くためtailを取る
 
-lstmLayers :: Bool     -- ^ True if BiLSTM, False otherwise
-  -> LstmParams        -- ^ a model
-  -> (Tensor,Tensor)   -- ^ a pair of initial tensors (h0,c0)
-  -> [Tensor]          -- ^ an input layer
-  -> [(Tensor,Tensor)] -- ^ the list of (hi,ci)
-lstmLayers ifBiLstm LstmParams{..} h0c0 inputs = 
-  let initialTensors = map (\(h,c) -> (toDependent h,toDependent c)) initialParams 
-      (firstParams:restParams) = lstmParams
-      firstLayer = lstmLayer ifBiLstm firstParams h0c0 inputs in
-  foldl' (\hicis nextLayer -> nextLayer $ snd $ unzip hicis)
-         firstLayer
-         (map (uncurry $ lstmLayer ifBiLstm) (zip restParams initialTensors))
+lstmLayers :: LstmHypParams -- ^ hyper params
+  -> LstmParams      -- ^ params
+  -> Tensor          -- ^ an input tensor of shape (L,H_in)
+  -> (Tensor,Tensor) -- ^ a pair of initial tensors: (D*num_layers,H_out)
+  -> IO Tensor       -- ^ [his] of shape (L,D*H_out)
+lstmLayers LstmHypParams{..} LstmParams{..} inputs (h0,c0) = do
+  let (h0h:h0t) = if bidirectional
+                    then [sliceDim 0 (2*i) (2*i+2) 1 h0 | i <- [0..num_layers]]
+                    else [sliceDim 0 i (i+1) 1 h0 | i <- [0..num_layers]]
+      (c0h:c0t) = if bidirectional
+                    then [sliceDim 0 (2*i) (2*i+2) 1 c0 | i <- [0..num_layers]]
+                    else [sliceDim 0 i (i+1) 1 c0 | i <- [0..num_layers]]
+  firstLayer <- singleLstmLayer bidirectional hidden_size firstLstmParams (h0h,c0h) $ unstack inputs
+  let restOfLayers = map (uncurry $ singleLstmLayer bidirectional hidden_size) $ zip restLstmParams $ zip h0t c0t
+      dropoutLayer = \hs -> case dropoutProb of
+                              Just prob -> dropout prob True hs
+                              Nothing -> return hs
+  foldl' (\hs nextLayer -> 
+           stack (Dim 0) <$> (nextLayer =<< (unstack <$> (dropoutLayer =<< hs))))
+         (return $ stack (Dim 0) firstLayer)
+         restOfLayers
 
-{-
-lstmLayers' :: Bool -- ^ if BiLSTM then True else False
-  -> LstmParams 
-  -> Tensor  -- ^ an input Tensor
-  -> Tensor　-- ^ the tensor of list of ci
-lstmLayers' ifBiLstm params inputs = 
-  stack (Dim 0) $ lstmLayers ifBiLstm params $ unstack inputs
--}
---
+++ src/Torch/Layer/SimpleLSTM.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Torch.Layer.SimpleLSTM (
+  LstmHypParams(..),
+  LstmParams(..),
+  lstmCell,
+  lstmLayers
+  ) where 
+
+import Prelude hiding (tanh) 
+import GHC.Generics              --base
+import Data.List (scanl',foldl') --base
+import Control.Monad (forM)      --base
+--hasktorch
+import Torch.Tensor       (Tensor(..))
+import Torch.Functional   (Dim(..),sigmoid,cat,stack)
+import Torch.Device       (Device(..))
+import Torch.NN           (Parameterized(..),Randomizable(..),Parameter,sample)
+import Torch.Autograd (IndependentTensor(..),makeIndependent)
+import Torch.Tensor.Util (unstack)
+import Torch.Tensor.TensorFactories (randnIO')
+import Torch.Layer.Linear (LinearHypParams(..),LinearParams(..),linearLayer)
+
+data LstmHypParams = LstmHypParams {
+  dev :: Device,
+  stateDim :: Int,
+  numOfLayers :: Int
+  } deriving (Eq, Show)
+
+data SingleLstmParams = SingleLstmParams {
+    forgetGate :: LinearParams,
+    inputGate :: LinearParams,
+    candidateGate :: LinearParams,
+    outputGate :: LinearParams
+    } deriving (Show, Generic)
+instance Parameterized SingleLstmParams
+
+data LstmParams = LstmParams {
+  lstmParams :: [SingleLstmParams],
+  initialParams :: [(Parameter,Parameter)]
+  } deriving (Show, Generic)
+
+instance Parameterized LstmParams
+instance Randomizable LstmHypParams LstmParams where
+  sample LstmHypParams{..} = do
+    let x_Dim = stateDim
+        h_Dim = stateDim
+        c_Dim = stateDim
+        xh_Dim = x_Dim + h_Dim
+    LstmParams
+      <$> forM [1..numOfLayers] (\_ ->
+        SingleLstmParams 
+          <$> sample (LinearHypParams dev xh_Dim c_Dim)
+          <*> sample (LinearHypParams dev xh_Dim c_Dim)
+          <*> sample (LinearHypParams dev xh_Dim c_Dim)
+          <*> sample (LinearHypParams dev xh_Dim h_Dim)
+          )
+      <*> forM [1..(numOfLayers-1)] (\_ ->
+        (\x y -> (x,y)) -- IO(a)->IO(b->(a,b))
+          <$> (makeIndependent =<< randnIO' dev [c_Dim]) 
+          <*> (makeIndependent =<< randnIO' dev [x_Dim])
+          )
+
+lstmCell :: SingleLstmParams 
+  -> (Tensor,Tensor) -- ^ (ct,ht) 
+  -> Tensor          -- ^ xt
+  -> (Tensor,Tensor) -- ^ (ct',ht')
+lstmCell SingleLstmParams{..} (ht,ct) xt =
+  let xt_ht = cat (Dim 0) [xt,ht]
+      ft = sigmoid $ linearLayer forgetGate $ xt_ht
+      it = sigmoid $ linearLayer inputGate $ xt_ht
+      cant = tanh' $ linearLayer candidateGate $ xt_ht
+      ct' = (ft * ct) + (it * cant)
+      ot = sigmoid $ linearLayer outputGate $ xt_ht
+      ht' = ot * (tanh' ct')
+  in (ht', ct')
+  where -- | HACK : Torch.Functional.tanhとexpが `Segmentation fault`になるため
+    tanh' x = 2 * (sigmoid $ 2 * x) - 1
+
+-- | inputのlistから、(cellState,hiddenState=output)のリストを返す
+-- | scanl' :: ((h,c) -> input -> (h',c')) -> (h0,c0) -> [input] -> [(hi,ci)]
+lstmLayer :: Bool       -- ^ if BiLSTM then True else False
+  -> SingleLstmParams   -- ^ hyper params
+  -> (Tensor,Tensor)    -- ^ A pair of vectors (h0,c0)
+  -> [Tensor]           -- ^ an input layer
+  -> [(Tensor,Tensor)]  -- ^ the list of (hi,ci) pairs
+lstmLayer ifBiLstm params (h0,c0) inputs = 
+  let firstLayer = tail $ scanl' (lstmCell params) (h0,c0) inputs in
+    if ifBiLstm -- | (c0,h0)は除くためtailを取る
+      then reverse $ tail $ scanl' (lstmCell params) (last firstLayer) $ reverse $ snd $ unzip firstLayer
+      else firstLayer
+
+lstmLayers :: Bool     -- ^ True if BiLSTM, False otherwise
+  -> LstmParams        -- ^ a model
+  -> (Tensor,Tensor)   -- ^ a pair of initial tensors (h0,c0)
+  -> [Tensor]          -- ^ an input layer
+  -> [(Tensor,Tensor)] -- ^ the list of (hi,ci)
+lstmLayers ifBiLstm LstmParams{..} h0c0 inputs = 
+  let initialTensors = map (\(h,c) -> (toDependent h,toDependent c)) initialParams 
+      (firstParams:restParams) = lstmParams
+      firstLayer = lstmLayer ifBiLstm firstParams h0c0 inputs in
+  foldl' (\hicis nextLayer -> nextLayer $ snd $ unzip hicis)
+         firstLayer
+         (map (uncurry $ lstmLayer ifBiLstm) (zip restParams initialTensors))
