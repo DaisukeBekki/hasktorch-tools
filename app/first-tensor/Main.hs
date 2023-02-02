d454fe6d175ea8e911de2ffb98a9a4e2e72b6457
Daisuke Bekki <bekki@is.ocha.ac.jp>
2023-02-02 13:57:45 +0900

    cleansed app/lstmSample/Main.hs

--- app/first-tensor/Main.hs
+++ app/first-tensor/Main.hs
@@ -1,15 +1,28 @@
 module Main where
 
 --hasktorch
-import Torch.Tensor (asValue)
+import Torch.Tensor (asValue,shape,dim,sliceDim)
 import Torch.Functional (add)
 import Torch.Device     (Device(..),DeviceType(..))
 --hasktorch-tools
-import Torch.Tensor.TensorFactories (asTensor'')
+import Torch.Tensor.TensorFactories (asTensor'',randnIO')
 
 main :: IO()
 main = do
-  let dev = Device CPU 0
-  let u = asTensor'' dev [1,2,3::Int]
+  let dev = Device CUDA 0
+      u = asTensor'' dev [[1,2,3],[2,3,4],[5,6,7],[8,9,10]::[Float]]
+      m = asTensor'' dev [[2,3],[4,5]::[Float]]
+  x <- randnIO' dev [1,3]
+  y <- randnIO' dev [2,3]
+  z <- randnIO' dev [3]
+  --when (shape u /= [2,2]) $ ioError (userError $ "illegal shape: " ++ (show $ shape u))
+  print x
+  print y
+  print $ dim x
+  print $ dim y
+  print $ dim z  
   print u
-
+  --print $ sliceDim 0 1 3 0 u
+  print $ head $ shape u
+  print $ sliceDim 0 0 1 1 u
+  print $ shape $ sliceDim 0 0 1 1 u
--- app/lstmSample/Main.hs
+++ app/lstmSample/Main.hs
@@ -6,7 +6,7 @@ module Main where
 
 import Control.Monad (when)
 --import System.IO.Unsafe (unsafePerformIO)
-import Torch.Tensor (shape,dim,sliceDim)
+import Torch.Tensor (shape)
 import Torch.Functional (mul,squeezeAll,mseLoss)
 import Torch.Device       (Device(..),DeviceType(..))
 import Torch.NN (sample)
@@ -19,35 +19,19 @@ import Torch.Layer.LSTM   (LstmHypParams(..),LstmParams(..),InitialStatesHypPara
 main :: IO()
 main = do
   let dev = Device CUDA 0
-  {-
-      u = asTensor'' dev [[1,2,3],[2,3,4],[5,6,7],[8,9,10]::[Float]]
-      m = asTensor'' dev [[2,3],[4,5]::[Float]]
-  x <- randnIO' dev [1,3]
-  y <- randnIO' dev [2,3]
-  z <- randnIO' dev [3]
-  --when (shape u /= [2,2]) $ ioError (userError $ "illegal shape: " ++ (show $ shape u))
-  print x
-  print y
-  print $ dim x
-  print $ dim y
-  print $ dim z  
-  print u
-  --print $ sliceDim 0 1 3 0 u
-  print $ head $ shape u
-  print $ sliceDim 0 0 1 1 u
-  print $ shape $ sliceDim 0 0 1 1 u
-  -}
-  let i_size = 2
-      h_size = 3
+      i_size = 2
+      h_size = 5
+      seq_len = 13
+      p_size = 1
       n_layers = 4
       isBiLSTM = True
   c0h0Params <- sample $ InitialStatesHypParams dev isBiLSTM h_size n_layers
-  let lstmHypParams = LstmHypParams dev isBiLSTM i_size h_size n_layers Nothing
+  let lstmHypParams = LstmHypParams dev isBiLSTM i_size h_size n_layers Nothing (Just p_size)
   lstmParams <- sample lstmHypParams
   let lstm = lstmLayers lstmHypParams lstmParams (c0h0s c0h0Params)
-  i <- randnIO' dev [13,2]
-  lstmout <- lstm i
-  gt <- randnIO' dev [13,6]
+  inputs <- randnIO' dev [seq_len,i_size]
+  lstmout <- lstm inputs
+  gt <- randnIO' dev [seq_len,p_size]
   let loss = mseLoss lstmout gt
   u <- update lstmParams GD loss 1e-1
   print u
