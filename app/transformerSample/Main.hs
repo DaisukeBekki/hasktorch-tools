41ff9facb848eb5947cf2d727fb7c8191806608a
Daisuke Bekki <bekki@is.ocha.ac.jp>
2023-02-22 13:04:07 +0900

    minor modifications

--- app/transformerSample/Main.hs
+++ app/transformerSample/Main.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses, DeriveGeneric, RecordWildCards #-}
+--{-# LANGUAGE TemplateHaskell #-}
+--{-# LANGUAGE FlexibleContexts #-}
+--{-# LANGUAGE FlexibleInstances #-}
 
 module Main where
 
@@ -6,17 +9,21 @@ import GHC.Generics               --base
 import Control.Monad      (forM_) --base
 --hasktorch 
 import Torch.Tensor       (shape,sliceDim)
-import Torch.Functional   (mseLoss,oneHot,embedding')
+import Torch.Functional   (Dim(..),mseLoss,transpose)
 import Torch.Device       (Device(..),DeviceType(..))
 import Torch.NN           (Parameterized(..),Randomizable(..),sample)
 import Torch.Optim        (GD(..))
 import Torch.Autograd     (IndependentTensor(..))
+--import Torch.Index        (slice)
 --hasktorch-tools
 import Torch.Train        (update)
 import Torch.Functional   (matmul)
 import Torch.Tensor.TensorFactories (asTensor'',randnIO')
 import Torch.Layer.Linear (LinearHypParams(..),LinearParams(..),linearLayer)
 import Torch.Layer.ProtoType.Transformer (TransformerHypParams(..),TransformerParams(..),sdpAttention)
+
+(..>) :: (a -> b) -> (b -> c) -> a -> c
+(..>) = flip fmap
 
 main :: IO ()
 main = do
@@ -34,6 +41,10 @@ main = do
   --u <- randnIO' dev [nBatches,seqLen,dimModel]
   --v <- randnIO' dev [nBatches,seqLen,dimV]
   --print $ sdpAttention model dimQK dimV dimModel nBatches nHeads seqLen u u v
-  m <- randnIO' dev [2,3,1,5]
-  x <- randnIO' dev [2,3,5,1]
-  print $ matmul m x
+  q <- randnIO' dev [2,2,3,2]
+  --k <- randnIO' dev [2,2,3,2]
+  print $ q
+  print $ sliceDim 2 1 2 1 q
+  let f = (sliceDim 2 1 2 1) ..> (transpose (Dim 2) (Dim 3)) 
+  print $ f q
+  print $ matmul (sliceDim 2 1 2 1 q) q
