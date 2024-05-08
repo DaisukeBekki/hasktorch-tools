db28647a5658e5d5407b2a60536dc65c2b9c5db8
Jossl123 <jossl1.gau@gmail.com>
2024-05-08 15:55:20 +0900

    add softmax activation function (because the one from hasktorch wasn't understandable)

--- src/Torch/Layer/NonLinear.hs
+++ src/Torch/Layer/NonLinear.hs
@@ -9,9 +9,9 @@ import Prelude hiding (tanh)
 import GHC.Generics              --base
 import qualified Data.Aeson as A --aeson
 import Torch.Tensor (Tensor(..)) --hasktorch
-import Torch.Functional   (sigmoid,relu,elu',selu) --hasktorch
+import Torch.Functional   (sigmoid,relu,elu',selu,div,exp,sumAll) --hasktorch
 
-data ActName = Id | Sigmoid | Tanh | Relu | Elu | Selu deriving (Eq,Show,Read,Generic)
+data ActName = Id | Sigmoid | Tanh | Relu | Elu | Selu | Softmax deriving (Eq,Show,Read,Generic)
 
 instance A.FromJSON ActName
 instance A.ToJSON ActName
@@ -20,9 +20,13 @@ decodeAct :: ActName -> Tensor -> Tensor
 decodeAct actname = case actname of
                    Id  -> id
                    Sigmoid -> sigmoid
+                   Softmax -> softmax
                    Tanh -> tanh
                    Relu -> relu
                    Elu -> elu'
                    Selu -> selu
   -- | HACK : Torch.Functional.tanhとexpが `Segmentation fault`になるため
   where tanh x = 2 * (sigmoid $ 2 * x) - 1
+        softmax x = Torch.Functional.div expInput expSum
+          where expInput =  Torch.Functional.exp x
+                expSum = sumAll expInput
