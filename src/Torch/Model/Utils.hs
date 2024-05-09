91404f9fa78700f4abdd1078a203e76299e4dbab
Jossl123 <jossl1.gau@gmail.com>
2024-05-09 13:51:54 +0900

    add accuracy precision recall f1 macroAvg weightedAvg

--- src/Torch/Model/Utils.hs
+++ src/Torch/Model/Utils.hs
@@ -1,10 +1,17 @@
 module Torch.Model.Utils (
-  confusionMatrix,
-  confusionMatrixPlot
+    confusionMatrix,
+    confusionMatrixPlot,
+    accuracy,
+    precision,
+    recall,
+    f1,
+    macroAvg,
+    weightedAvg
   ) where
 
-import Torch.Functional (add)
+import Torch.Functional (add, mul, div, sumAll)
 import Torch.Tensor (Tensor(..), asTensor, asValue)
+import Torch.TensorFactories (zeros')
 import Torch.Tensor.TensorFactories (oneAtPos2d)
 import Torch.Tensor.Util (indexOfMax,oneHot')
 import Torch.Layer.MLP      (MLPParams(..))
@@ -44,3 +51,74 @@ confusionMatrixPlot m labels =
         % ylabel "Expected" 
         % colorbar 
         % mp # ("ax.invert_yaxis()" :: String)) labelsText) valuesText
+
+-- | Return the accuracy of a model
+-- * model : The model
+-- * forward : The forward function for the model
+-- * dataSet : The dataset
+accuracy :: MLPParams -> (MLPParams -> Tensor -> Tensor) -> [(Tensor,Tensor)] -> Float
+accuracy model forward dataSet = (sum results) / (fromIntegral (length results))
+    where results = map (\(input, output) -> if (indexOfMax $ (asValue (forward model input) :: [Float])) == (indexOfMax $ (asValue output :: [Float])) then 1 else 0) dataSet
+       
+-- | Return the precision of a model
+-- * model : The model
+-- * forward : The forward function for the model
+-- * dataSet : The dataset
+precision :: MLPParams -> (MLPParams -> Tensor -> Tensor) -> [(Tensor,Tensor)] -> Tensor
+precision model forward dataSet = Torch.Functional.div tp (Torch.Functional.add tp fp)
+    where (tp, _, fp) = getTpFnFp model forward dataSet
+
+-- | Return the recall of a model
+-- * model : The model
+-- * forward : The forward function for the model
+-- * dataSet : The dataset
+recall :: MLPParams -> (MLPParams -> Tensor -> Tensor) -> [(Tensor,Tensor)] -> Tensor
+recall model forward dataSet = Torch.Functional.div tp (Torch.Functional.add tp fn)
+    where (tp, fn, _) = getTpFnFp model forward dataSet
+
+-- | Return the f1 score of a model
+-- * model : The model
+-- * forward : The forward function for the model
+-- * dataSet : The dataset
+f1 :: MLPParams -> (MLPParams -> Tensor -> Tensor) -> [(Tensor,Tensor)] -> Tensor
+f1 model forward dataSet = Torch.Functional.div tp (Torch.Functional.add tp (Torch.Functional.div (Torch.Functional.add fn fp) 2.0))
+    where (tp, fn, fp) = getTpFnFp model forward dataSet
+
+-- | Return the macro average of the f1 score of a model
+-- * model : The model
+-- * forward : The forward function for the model
+-- * dataSet : The dataset
+macroAvg :: MLPParams -> (MLPParams -> Tensor -> Tensor) -> [(Tensor,Tensor)] -> Float
+macroAvg model forward dataSet = (asValue (sumAll f1score) :: Float) / (fromIntegral $ length (asValue f1score :: [Float]))
+    where f1score = f1 model forward dataSet
+
+-- | Return the weighted average of the f1 score of a model
+-- * model : The model
+-- * forward : The forward function for the model
+-- * dataSet : The dataset
+weightedAvg :: MLPParams -> (MLPParams -> Tensor -> Tensor) -> [(Tensor,Tensor)] -> Float
+weightedAvg model forward dataSet = result
+    where expecteds = map snd dataSet
+          weights = Torch.Functional.div (foldl1 (Torch.Functional.add) expecteds) (fromIntegral $ length dataSet) 
+          f1score = f1 model forward dataSet
+          weightedF1 = Torch.Functional.mul weights f1score
+          result = asValue (sumAll weightedF1) :: Float
+
+
+----
+
+-- | Return the true positive, false negative and false positive of one class
+getOneTpFnFp :: Tensor -> Tensor -> (Tensor,Tensor,Tensor)
+getOneTpFnFp expected guess = result
+    where expectedValue = asValue expected :: [Float]
+          guessValue = asValue guess :: [Float] 
+          nul = zeros' [(length (asValue expected :: [Float]))]
+          result = if indexOfMax expectedValue == indexOfMax guessValue then (expected, nul, nul) else (nul, expected, guess)
+
+-- | Return the true positive, false negative and false positive of a classification model
+getTpFnFp :: MLPParams -> (MLPParams -> Tensor -> Tensor) -> [(Tensor,Tensor)] -> (Tensor,Tensor,Tensor)
+getTpFnFp model forward dataSet = (tp, fn, fp)
+    where fTpFnFp = [ getOneTpFnFp output $ oneHot' $ forward model input | (input, output) <- dataSet]
+          tp = foldl1 (Torch.Functional.add) [tps | (tps, _, _) <- fTpFnFp]
+          fn = foldl1 (Torch.Functional.add) [fns | (_, fns, _) <- fTpFnFp]
+          fp = foldl1 (Torch.Functional.add) [fps | (_, _, fps) <- fTpFnFp]
