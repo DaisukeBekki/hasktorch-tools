1b47cd7d7d9ff98e1c7c319146020c63a00ca5c5
Caetano Godinat <caetano.godinat@hotmail.com>
2024-06-12 21:53:08 +0900

    saveState function

--- src/Torch/Train.hs
+++ src/Torch/Train.hs
@@ -4,19 +4,22 @@ module Torch.Train (
   zeroTensor,
   sumTensors,
   saveParams,
-  loadParams
+  loadParams,
+  saveState
   ) where
 
 import Control.Monad (when)     --base
 import Text.Printf   (printf)   --base
 
-import Torch.Tensor    (Tensor(..),reshape)
+import Torch.Tensor    (Tensor(..),reshape, asTensor, asValue)
 import Torch.TensorFactories (zeros')
 import Torch.Functional (Dim(..),sumAll,cat)
-import Torch.NN        (Parameterized(..),Randomizable(..),replaceParameters)
+import Torch.NN        (Parameterized(..),Randomizable(..),replaceParameters, Parameter)
 import Torch.Autograd  (IndependentTensor(..),makeIndependent,grad)
-import Torch.Optim     (Optimizer(..),Gradients(..))
-import Torch.Serialize (save,load)
+import Torch.Optim     (Optimizer(..),Gradients(..), Adam(..))
+import Torch.Serialize ( save, load, pickleSave, pickleLoad )
+import Torch.Script (IValue(..))
+import Torch.Autograd (IndependentTensor(..))
 
 type Loss = Tensor
 type LearningRate = Tensor
@@ -31,8 +34,8 @@ update model opt lossTensor learningRate = do
   return (replaceParameters model newParams, opt')
 
 showLoss :: Int -> Int -> Float -> IO()
-showLoss modulus epoc lossValue = 
-  when (epoc `mod` modulus == 0) $ 
+showLoss modulus epoc lossValue =
+  when (epoc `mod` modulus == 0) $
     putStrLn $ "Epoc: " ++ printf "%4d" epoc ++ "  --->  Loss: " ++ printf "%.6f" lossValue
 
 zeroTensor :: Tensor
@@ -42,7 +45,7 @@ sumTensors :: [Tensor] -> Tensor
 sumTensors ts = sumAll $ cat (Dim 0) $ map (reshape [1]) ts
 
 saveParams :: (Parameterized param) => param -> FilePath -> IO()
-saveParams params = save (map toDependent $ flattenParameters params) 
+saveParams params = save (map toDependent $ flattenParameters params)
 
 loadParams :: (Parameterized param, Randomizable hyp param) => hyp -> FilePath -> IO(param)
 loadParams hyps filepath = do
@@ -51,3 +54,16 @@ loadParams hyps filepath = do
   model <- sample hyps
   return $ replaceParameters model loaded_params
 
+saveState :: Parameterized model => Int -> model -> Adam -> Tensor -> FilePath -> IO ()
+saveState epoc model opt loss path = do
+  let flattenParams = map toDependent $ flattenParameters model
+  independentLoss <- makeIndependent loss
+  let Adam {beta1 = b1, beta2 = b2, m1 = m1List, m2 = m2List, iter = iteration} = opt
+
+  let lossEpoc = IVTensorList [asTensor (fromIntegral epoc :: Double) :: Tensor, toDependent independentLoss]
+  let modelFlattened = IVTensorList flattenParams
+  let betasIteration = IVTensorList [asTensor b1 :: Tensor, asTensor b2 :: Tensor, asTensor (fromIntegral iteration :: Double)]
+  let m1 = IVTensorList m1List
+  let m2 = IVTensorList m2List
+  let state = IVGenericDict [(IVString "model", modelFlattened), (IVString "loss", lossEpoc), (IVString "betasIteration", betasIteration), (IVString "m1", m1), (IVString "m2", m2)]
+  pickleSave state path
